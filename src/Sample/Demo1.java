77398069b78261213f3b88deee233e074c586c0c
sandh <sandh@DESKTOP-7A9FBF0>
2024-07-04 18:22:21 +0100

    [Rinkoo]:created class Demo1

---
+++ src/Sample/Demo1.java
@@ -0,0 +1,21 @@
+package Sample;
+
+public class Demo1 
+{
+    public static void main(String[] args) 
+    {
+		System.out.println("Hi");
+	    System.out.println("Helo");
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+}
